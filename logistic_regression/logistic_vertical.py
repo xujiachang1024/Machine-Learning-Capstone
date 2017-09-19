82a6aaa92afc02f13749603bca32136ef47bf611
Jiachang Xu <jiachanx@usc.edu>
2017-09-19 16:41:07 -0700

    Row Iterator Loop

--- logistic_regression/logistic_vertical.py
+++ logistic_regression/logistic_vertical.py
@@ -51,3 +51,25 @@ params = np.zeros((params_num, 1))  # a column vector with shape (paras_num, 1)
 params[:, 0] = np.random.uniform(low=-0.5, high=0.5, size=(params_num,))  # fill params with random [-0.5, 0.5)
 
 
+# Loop for 1,000 iterations
+for i in range(epochs):
+
+    # shuffle the test set (X-variables)
+    random.shuffle(test_X)
+
+    sig_out = [0] * train_X.shape[0]  # sigmoid output array (predicted Y)
+    diff = [0] * train_X.shape[0] # difference between sigmoid output (predicted Y) and actual Y from the train set
+    gradient = np.zeros((train_X.shape[1], 1))  # a column vector with shape (train_rows, 1)
+    data = np.zeros((train_X.shape[1], train_X.shape[0]))  # a zero matrix with shape (train_cols, train_rows)
+    sig_der = np.zeros((train_X.shape[0], train_X.shape[0]))  # a zero matrix with shape (train_rows, train_rows)
+
+    # iterate through each row in the train set
+    for j in range(train_X.shape[0]):
+
+        # compute sigmoid outputs
+        sig_out[j] = sigmoid(np.dot(train_X[j], params[:, ]))
+        diff[j] = sig_out[j] - train_Y[j]
+
+        # compute gradient vector of negative log likelihood
+        data[:, j] = train_X[j].transpose()
+        gradient[:, 0] = gradient[:, 0] + np.multiply(train_X[j].transpose(), diff[j])
