d315a82d5b04df781dbc71daa7a1f7e40fbd744d
Jiachang Xu <jiachanx@usc.edu>
2017-09-26 23:20:10 -0700

    Decision Tree Classifier

--- sklearn/sklearn_DTClassifier.py
+++ sklearn/sklearn_DTClassifier.py
@@ -2,6 +2,9 @@
 import random
 import pandas as pd
 import numpy as np
+import warnings
+from scipy import stats
+from sklearn.exceptions import UndefinedMetricWarning
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn import metrics
@@ -9,6 +12,7 @@ from sklearn import metrics
 
 # Set the seed (reproducibility)
 random.seed(0)
+warnings.filterwarnings('ignore', category=UndefinedMetricWarning)
 
 
 # Data import and cleaning
@@ -68,14 +72,6 @@ for i in range(0, cv, 1):
     # calculate the confusion matrix
     matrix = metrics.confusion_matrix(test_Y, predicted_Y)
 
-    # print iterative result
-    # print('\n-----------------------------------')
-    # print('Iteration ', i)
-    # print('Features: speed, X-accel, Y-accel, Z-accel, Z-jolt')
-    # print('Labels: speedbump (1 = yes, 0 = no)')
-    # print('F1 score:', f1)
-    # print(matrix)
-
 
 # Calculate cross-validation average
 print('\n-----------------------------------')
@@ -83,6 +79,11 @@ print('sklearn.tree.DecisionTreeClassifier Model 1')
 print('\tFeatures: speed, X-accel, Y-accel, Z-accel, Z-jolt')
 print('\tLabels: speedbump (1 = yes, 0 = no)')
 print('\tAverage F1 score:', np.mean(f1_scores))
+print('\tStdDev F1 score:', np.std(f1_scores))
+print('\tMedian F1 score:', np.median(f1_scores))
+print('\tIQR F1 score:', stats.iqr(f1_scores))
+print('\tSkewness F1 score:', stats.skew(f1_scores))
+
 
 
 # Decision Tree Model 2
@@ -118,14 +119,6 @@ for i in range(0, cv, 1):
     # calculate the confusion matrix
     matrix = metrics.confusion_matrix(test_Y, predicted_Y)
 
-    # print iterative result
-    # print('\n-----------------------------------')
-    # print('Iteration ', i)
-    # print('Features: speed, X-accel, Y-accel, Z-accel')
-    # print('Labels: speedbump (1 = yes, 0 = no)')
-    # print('F1 score:', f1)
-    # print(matrix)
-
 
 # Calculate cross-validation average
 print('\n-----------------------------------')
@@ -133,6 +126,10 @@ print('sklearn.tree.DecisionTreeClassifier Model 2')
 print('\tFeatures: speed, X-accel, Y-accel, Z-accel')
 print('\tLabels: speedbump (1 = yes, 0 = no)')
 print('\tAverage F1 score:', np.mean(f1_scores))
+print('\tStdDev F1 score:', np.std(f1_scores))
+print('\tMedian F1 score:', np.median(f1_scores))
+print('\tIQR F1 score:', stats.iqr(f1_scores))
+print('\tSkewness F1 score:', stats.skew(f1_scores))
 
 
 # Decision Tree Model 3
@@ -168,14 +165,6 @@ for i in range(0, cv, 1):
     # calculate the confusion matrix
     matrix = metrics.confusion_matrix(test_Y, predicted_Y)
 
-    # print iterative result
-    # print('\n-----------------------------------')
-    # print('Iteration ', i)
-    # print('Features: speed, X-accel, Y-accel, Z-jolt')
-    # print('Labels: speedbump (1 = yes, 0 = no)')
-    # print('F1 score:', f1)
-    # print(matrix)
-
 
 # Calculate cross-validation average
 print('\n-----------------------------------')
@@ -183,3 +172,53 @@ print('sklearn.tree.DecisionTreeClassifier Model 3')
 print('\tFeatures: speed, X-accel, Y-accel, Z-jolt')
 print('\tLabels: speedbump (1 = yes, 0 = no)')
 print('\tAverage F1 score:', np.mean(f1_scores))
+print('\tStdDev F1 score:', np.std(f1_scores))
+print('\tMedian F1 score:', np.median(f1_scores))
+print('\tIQR F1 score:', stats.iqr(f1_scores))
+print('\tSkewness F1 score:', stats.skew(f1_scores))
+
+
+# Decision Tree Model 4
+# Separate Y and X variables
+df_label = df.loc[:, 'speedbump']
+df_feature = df.loc[:, ('X', 'Y', 'Z', 'z_jolt')]
+Y = df_label.as_matrix()
+X = df_feature.as_matrix()
+
+
+# Prepare for cross-validation
+clf = DecisionTreeClassifier()  # create a DecisionTreeClassifier
+f1_scores = []  # sum of F1 scores
+cv = 100;  # number of cross-validations
+
+
+# Start cross-validation
+for i in range(0, cv, 1):
+
+    # split to train and test sets
+    train_X, test_X, train_Y, test_Y = train_test_split(X, Y, test_size=0.2, shuffle=True)
+
+    # start training
+    clf = clf.fit(train_X, train_Y)  # fit the training data
+
+    # start testing
+    predicted_Y = clf.predict(test_X)  # predict on the testing data
+
+    # calculate the F1 score
+    f1 = metrics.f1_score(test_Y, predicted_Y, average='binary')  # calculate the F1 score
+    f1_scores.append(f1)
+
+    # calculate the confusion matrix
+    matrix = metrics.confusion_matrix(test_Y, predicted_Y)
+
+
+# Calculate cross-validation average
+print('\n-----------------------------------')
+print('sklearn.tree.DecisionTreeClassifier Model 4')
+print('\tFeatures: X-accel, Y-accel, Z-accel, Z-jolt')
+print('\tLabels: speedbump (1 = yes, 0 = no)')
+print('\tAverage F1 score:', np.mean(f1_scores))
+print('\tStdDev F1 score:', np.std(f1_scores))
+print('\tMedian F1 score:', np.median(f1_scores))
+print('\tIQR F1 score:', stats.iqr(f1_scores))
+print('\tSkewness F1 score:', stats.skew(f1_scores))
