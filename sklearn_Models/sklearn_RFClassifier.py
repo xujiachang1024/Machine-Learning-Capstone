e8b858fa4446d162f3781c13ca401a624d1b3c8c
Jiachang Xu <jiachanx@usc.edu>
2017-09-28 15:00:16 -0700

    Random Forest Model on Kenya data

--- sklearn_Models/sklearn_RFClassifier.py
+++ sklearn_Models/sklearn_RFClassifier.py
@@ -16,35 +16,20 @@ warnings.filterwarnings('ignore', category=UndefinedMetricWarning)
 
 
 # Data import and cleaning
-df1 = pd.read_csv("./speedbumps_1.csv")  # read data from the .csv file
-df2 = pd.read_csv("./speedbumps_2.csv")  # read data from the .csv file
-df3 = pd.read_csv("./speedbumps_3.csv")  # read data from the .csv file
-df4 = pd.read_csv("./speedbumps_4.csv")  # read data from the .csv file
-df = pd.read_csv("./speedbumps_5.csv")  # read data from the .csv file
-df1 = df1.loc[:, ('speedbump', 'Speed', 'X', 'Y', 'Z', 'z_jolt')]  # only select relevant columns
-df2 = df2.loc[:, ('speedbump', 'Speed', 'X', 'Y', 'Z', 'z_jolt')]  # only select relevant columns
-df3 = df3.loc[:, ('speedbump', 'Speed', 'X', 'Y', 'Z', 'z_jolt')]  # only select relevant columns
-df4 = df4.loc[:, ('speedbump', 'Speed', 'X', 'Y', 'Z', 'z_jolt')]  # only select relevant columns
-df = df.loc[:, ('speedbump', 'Speed', 'X', 'Y', 'Z', 'z_jolt')]  # only select relevant columns
-df = df.append(df1)
-df = df.append(df2)
-df = df.append(df3)
-df = df.append(df4)
-keywords = ['yes', 'no']
-mapping = [1, 0]
-df = df.replace(keywords, mapping)
+df = pd.read_csv("./kenya_jolt_2.csv")  # read data from the .csv file
+df = df.loc[:, ('speedbump', 'x', 'y', 'z', 'x_jolt', 'y_jolt', 'z_jolt')]  # only select relevant columns
 
 
 # Random Forest Model 1
 # Separate Y and X variables
 df_label = df.loc[:, 'speedbump']
-df_feature = df.loc[:, ('Speed', 'X', 'Y', 'Z', 'z_jolt')]
+df_feature = df.loc[:, ('x', 'y', 'z', 'x_jolt', 'y_jolt', 'z_jolt')]
 Y = df_label.as_matrix()
 X = df_feature.as_matrix()
 
 
 # Prepare for cross-validation
-clf = RandomForestClassifier(random_state=0)  # create a MLPClassifier
+clf = RandomForestClassifier()  # create a DecisionTreeClassifier
 f1_scores = []  # sum of F1 scores
 cv = 100  # number of cross-validations
 
@@ -71,8 +56,8 @@ for i in range(0, cv, 1):
 
 # Calculate cross-validation average
 print('\n-----------------------------------')
-print('sklearn_Models.ensemble.RandomForestClassifier Model 1')
-print('\tFeatures: speed, X-accel, Y-accel, Z-accel, Z-jolt')
+print('sklearn.ensemble.RandomForestClassifier Model 1')
+print('\tFeatures: X-accel, Y-accel, Z-accel, X-jolt, Y-jolt, Z-jolt')
 print('\tLabels: speedbump (1 = yes, 0 = no)')
 print('\tAverage F1 score:', np.mean(f1_scores))
 print('\tStdDev F1 score:', np.std(f1_scores))
@@ -84,13 +69,13 @@ print('\tSkewness F1 score:', stats.skew(f1_scores))
 # Random Forest Model 2
 # Separate Y and X variables
 df_label = df.loc[:, 'speedbump']
-df_feature = df.loc[:, ('Speed', 'X', 'Y', 'Z')]
+df_feature = df.loc[:, ('x', 'y', 'z', 'y_jolt', 'z_jolt')]
 Y = df_label.as_matrix()
 X = df_feature.as_matrix()
 
 
 # Prepare for cross-validation
-clf = RandomForestClassifier(random_state=0)  # create a MLPClassifier
+clf = RandomForestClassifier()  # create a DecisionTreeClassifier
 f1_scores = []  # sum of F1 scores
 cv = 100  # number of cross-validations
 
@@ -117,8 +102,8 @@ for i in range(0, cv, 1):
 
 # Calculate cross-validation average
 print('\n-----------------------------------')
-print('sklearn_Models.ensemble.RandomForestClassifier Model 2')
-print('\tFeatures: speed, X-accel, Y-accel, Z-accel')
+print('sklearn.ensemble.RandomForestClassifier Model 2')
+print('\tFeatures: X-accel, Y-accel, Z-accel, Y-jolt, Z-jolt')
 print('\tLabels: speedbump (1 = yes, 0 = no)')
 print('\tAverage F1 score:', np.mean(f1_scores))
 print('\tStdDev F1 score:', np.std(f1_scores))
@@ -130,15 +115,15 @@ print('\tSkewness F1 score:', stats.skew(f1_scores))
 # Random Forest Model 3
 # Separate Y and X variables
 df_label = df.loc[:, 'speedbump']
-df_feature = df.loc[:, ('Speed', 'X', 'Y', 'z_jolt')]
+df_feature = df.loc[:, ('y', 'z', 'x_jolt', 'y_jolt', 'z_jolt')]
 Y = df_label.as_matrix()
 X = df_feature.as_matrix()
 
 
 # Prepare for cross-validation
-clf = RandomForestClassifier(random_state=0)  # create a MLPClassifier
+clf = RandomForestClassifier()  # create a DecisionTreeClassifier
 f1_scores = []  # sum of F1 scores
-cv = 100  # number of cross-validations
+cv = 100;  # number of cross-validations
 
 
 # Start cross-validation
@@ -163,54 +148,8 @@ for i in range(0, cv, 1):
 
 # Calculate cross-validation average
 print('\n-----------------------------------')
-print('sklearn_Models.ensemble.RandomForestClassifier Model 3')
-print('\tFeatures: speed, X-accel, Y-accel, Z-jolt')
-print('\tLabels: speedbump (1 = yes, 0 = no)')
-print('\tAverage F1 score:', np.mean(f1_scores))
-print('\tStdDev F1 score:', np.std(f1_scores))
-print('\tMedian F1 score:', np.median(f1_scores))
-print('\tIQR F1 score:', stats.iqr(f1_scores))
-print('\tSkewness F1 score:', stats.skew(f1_scores))
-
-
-# Random Forest Model 4
-# Separate Y and X variables
-df_label = df.loc[:, 'speedbump']
-df_feature = df.loc[:, ('X', 'Y', 'Z', 'z_jolt')]
-Y = df_label.as_matrix()
-X = df_feature.as_matrix()
-
-
-# Prepare for cross-validation
-clf = RandomForestClassifier(random_state=0)  # create a MLPClassifier
-f1_scores = []  # sum of F1 scores
-cv = 100  # number of cross-validations
-
-
-# Start cross-validation
-for i in range(0, cv, 1):
-
-    # split to train and test sets
-    train_X, test_X, train_Y, test_Y = train_test_split(X, Y, test_size=0.2, shuffle=True)
-
-    # start training
-    clf = clf.fit(train_X, train_Y)  # fit the training data
-
-    # start testing
-    predicted_Y = clf.predict(test_X)  # predict on the testing data
-
-    # calculate the F1 score
-    f1 = metrics.f1_score(test_Y, predicted_Y, average='binary')  # calculate the F1 score
-    f1_scores.append(f1)
-
-    # calculate the confusion matrix
-    matrix = metrics.confusion_matrix(test_Y, predicted_Y)
-
-
-# Calculate cross-validation average
-print('\n-----------------------------------')
-print('sklearn_Models.ensemble.RandomForestClassifier Model 4')
-print('\tFeatures: X-accel, Y-accel, Z-accel, Z-jolt')
+print('sklearn.ensemble.RandomForestClassifier Model 3')
+print('\tFeatures: Y-accel, Z-accel, X-jolt, Y-jolt, Z-jolt')
 print('\tLabels: speedbump (1 = yes, 0 = no)')
 print('\tAverage F1 score:', np.mean(f1_scores))
 print('\tStdDev F1 score:', np.std(f1_scores))
